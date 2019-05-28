9bb8f4d1371a00d57e3cacb451fe9487afc346af
Daniel Lemire <lemire@gmail.com>
2019-05-28 15:18:20 -0400

    Credit: Stuart Carnie.

--- Sources/SwiftWyhash/SwiftWyhash.swift
+++ Sources/SwiftWyhash/SwiftWyhash.swift
@@ -1,20 +1,20 @@
-class WyhashGenerator {
-    var seed : UInt64
+struct WyhashGenerator {
+    private var seed : UInt64
 
-    let multiplier1 : UInt64 = 0xa3b195354a39b70d
-    let multiplier2 : UInt64 = 0x1b03738712fad5c9
-    let increment : UInt64 = 0x60bee2bee120fc15
+    private static let multiplier1 : UInt64 = 0xa3b195354a39b70d
+    private static let multiplier2 : UInt64 = 0x1b03738712fad5c9
+    private static let increment : UInt64 = 0x60bee2bee120fc15
 
     init(userSeed : UInt64) {
         seed = userSeed;
     }
 
-    func random() -> UInt64 {
-        seed &+= increment
-        let fullmult1 = seed.multipliedFullWidth(by: multiplier1)
+    mutating func random() -> UInt64 {
+        seed &+= WyhashGenerator.increment
+        let fullmult1 = seed.multipliedFullWidth(by: WyhashGenerator.multiplier1)
         let m1 = fullmult1.high ^ fullmult1.low;
-        let fullmult2 = m1.multipliedFullWidth(by: multiplier2)
+        let fullmult2 = m1.multipliedFullWidth(by: WyhashGenerator.multiplier2)
         let m2 = fullmult2.high ^ fullmult2.low;
-        return m2
+        return m2;
     }
 }
--- Tests/SwiftWyhashTests/SwiftWyhashTests.swift
+++ Tests/SwiftWyhashTests/SwiftWyhashTests.swift
@@ -3,7 +3,7 @@ import XCTest
 
 final class SwiftWyhashTests: XCTestCase {
     func testBasic() {
-        let gen = WyhashGenerator(userSeed: 0)
+        var gen = WyhashGenerator(userSeed: 0)
         XCTAssertEqual(gen.random(), 6661202149082483300)
         XCTAssertEqual(gen.random(), 13322404298164966600)
         XCTAssertEqual(gen.random(), 10710867605997789043)
