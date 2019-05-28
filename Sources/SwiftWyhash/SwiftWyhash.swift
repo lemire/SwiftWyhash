98a183df7286fa907365c4ebfe0c1cb479c1f67b
Daniel Lemire <lemire@gmail.com>
2019-05-28 13:08:00 -0400

    Update SwiftWyhash.swift

--- Sources/SwiftWyhash/SwiftWyhash.swift
+++ Sources/SwiftWyhash/SwiftWyhash.swift
@@ -1,13 +1,3 @@
-/*uint64_t wyhash64_stateless(uint64_t *seed) {
-  *seed += UINT64_C(0x60bee2bee120fc15);
-  __uint128_t tmp;
-  tmp = (__uint128_t)*seed * UINT64_C(0xa3b195354a39b70d);
-  uint64_t m1 = (tmp >> 64) ^ tmp;
-  tmp = (__uint128_t)m1 * UINT64_C(0x1b03738712fad5c9);
-  uint64_t m2 = (tmp >> 64) ^ tmp;
-  return m2;
-}*/
-
 class WyhashGenerator {
     var seed : UInt64
 
