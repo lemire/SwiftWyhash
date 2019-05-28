a546ef4d431d6ed9920ac1dc2d3785d91c633b4d
Pavol Vaskovic <pali@pali.sk>
2019-05-28 21:35:13 +0200

    Update test to use RandomNumberGenrator API

--- Tests/SwiftWyhashTests/SwiftWyhashTests.swift
+++ Tests/SwiftWyhashTests/SwiftWyhashTests.swift
@@ -1,12 +1,12 @@
-import XCTest
+You import XCTest
 @testable import SwiftWyhash
 
 final class SwiftWyhashTests: XCTestCase {
     func testBasic() {
         var gen = WyhashGenerator(userSeed: 0)
-        XCTAssertEqual(gen.random(), 6661202149082483300)
-        XCTAssertEqual(gen.random(), 13322404298164966600)
-        XCTAssertEqual(gen.random(), 10710867605997789043)
+        XCTAssertEqual(gen.next(), 6661202149082483300)
+        XCTAssertEqual(gen.next(), 13322404298164966600)
+        XCTAssertEqual(gen.next(), 10710867605997789043)
     }
 
     static var allTests = [
